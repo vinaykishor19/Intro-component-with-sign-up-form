1a55c4485dfe9ca20563fbcfa4f4c1fe79fca53a
vinaykishor19 <95738913+vinaykishor19@users.noreply.github.com>
2022-10-12 11:39:24 +0530

    editted

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputs = document.querySelectorAll("input:not([type='submit'])");
+
+inputs.forEach(input => {
+    input.addEventListener('invalid', addErrorMessage);
+
+    input.addEventListener('blur', (event) => {
+        input.checkValidity();
+    })
+
+    // Remove existing error messages on focus
+    input.addEventListener('focus', removeErrorMessage);
+})
+
+function addErrorMessage(e) {
+    var name = e.target.getAttribute("name");
+
+    // Create an error icon element.
+    var error_icon = document.createElement('span');
+    error_icon.setAttribute("data-id", name);
+    error_icon.classList.add('error-icon');
+    error_icon.innerHTML = "<img src='images/icon-error.svg' alt=''>";
+
+    // Create an error message.
+    var error_message = document.createElement('span');
+    error_message.setAttribute("data-id", name);
+    error_message.classList.add('error-message');
+
+    if (e.target.value == "" || e.target.value == null) error_message.innerHTML = "" + e.target.getAttribute("placeholder") + " cannot be empty.";
+    else error_message.innerHTML = "Looks like this is not an " + e.target.getAttribute("placeholder") + ".";
+
+    e.target.after(error_message);
+    e.target.after(error_icon);
+    e.target.classList.add('error');
+}
+
+function removeErrorMessage(e) {
+    var elements = document.querySelectorAll("[data-id='"+ e.target.getAttribute("name")+"']");
+
+    // Remove those elements.
+    elements.forEach(error_element => {
+        error_element.remove();
+    })
+
+    e.target.classList.remove('error');
+}
